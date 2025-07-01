bc8c75c1482bc2a3de5bc88b6640acaf14a4e781
Student-UETTaxila <ammarahussain2024@gmail.com>
2025-07-01 08:11:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+//Getting elements from the Dom
+const ratings=document.querySelectorAll(".ratings");
+const ratingsContainer=document.querySelector(".ratings-container");
+const sendBtn = document.querySelector("#send");
+const panel=document.querySelector("#panel");
+let selectedRating="Satisfied";
+//attach events
+ratingsContainer.addEventListener("click",e=>{
+    if(e.target.parentNode.classList.contains("rating")){
+        //console.log(e.target.parentNode.classList.contains("rating"));
+        removeActive()
+        e.target.parentNode.classList.add("active");
+        selectedRating=e.target.nextElementSibling.innerHTML;
+    }
+});
+sendBtn.addEventListener('click',()=>{
+    panel.innerHTML=`<p class="heart">💖</p>
+<strong>Thank You!</strong>
+<br>
+<strong>Feedback: ${selectedRating}</strong>`
+
+});
+function removeActive(){
+    for(let i=0;i<ratings.length;i++){
+        ratings[i].classList.remove("active");
+    }
+}
+
